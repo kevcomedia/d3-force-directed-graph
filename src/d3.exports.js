5acbfcc36c225c2fae16fb57d1e84557fb5d079c
Kev Comedia <kevcomedia@gmail.com>
2017-09-28 18:58:49 +0800

    Remove unused D3 modules

--- src/d3.exports.js
+++ src/d3.exports.js
@@ -4,9 +4,7 @@
  *
  * Export only from the modules that you need to keep the bundle size minimal.
  */
-export {max, min} from 'd3-array';
 export * from 'd3-drag';
 export * from 'd3-force';
 export * from 'd3-request';
-export {scaleLinear} from 'd3-scale';
 export * from 'd3-selection';
